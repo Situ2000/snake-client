275ef3795a2c6b74b8a2eb5dfc4c2b501c7dbe4e
Situ2000 <yuanyuansitu@gmail.com>
2022-11-19 06:55:15 +0000

    Press WASD to print out four direction

--- input.js
+++ input.js
@@ -15,9 +15,26 @@ const setupInput = function (conn) {
 };
 
 // terminate the game using ctrl + c
+// Press WASD to move four direction
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+
+  if (key === 'w') {
+    console.log("Move: up");
+  }
+
+  if (key === 'a') {
+    console.log("Move: left");
+  }
+
+  if (key === 's') {
+    console.log("Move: down");
+  }
+
+  if (key === 'd') {
+    console.log("Move: right");
   }
 };
 
